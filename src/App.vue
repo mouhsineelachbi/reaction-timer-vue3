abfc4b840116079586504d613031a78265c7c0a8
mouhsineelachbi <mouhsineelachbi@gmail.com>
2020-12-13 22:09:10 +0100

    show result component with reaction time by biding the score prop to result component

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,19 @@
   <h1>Mythose Reaction Timer</h1>  
   <button @click="start" :disabled="isPlaying">Play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
-  <p v-if="showResult"> Reaction Time {{ score }} ms</p>
+  <!--<p v-if="showResult"> Reaction Time {{ score }} ms</p>-->
+  <Result v-if="showResult" :score="this.score" />
 </template>
 
 <script>
 // Imports
 import Block from './components/Block.vue'
+import Result from './components/Result.vue'
 
 
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Result },
   data(){
     return {
       isPlaying: false,
---
+++ src/components/Result.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p> Reaction Time : {{ score }} ms</p>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    props: ['score']
+
+}
+</script>
+
+<style>
+
+</style>
