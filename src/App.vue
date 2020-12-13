f403a105f78be708dfc6d03bcad783b0cb667afd
mouhsineelachbi <mouhsineelachbi@gmail.com>
2020-12-13 22:00:58 +0100

    customized events with data for passing data from block component to result through app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>Mythose Reaction Timer</h1>  
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay"/>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <p v-if="showResult"> Reaction Time {{ score }} ms</p>
 </template>
 
 <script>
@@ -15,13 +16,21 @@ export default {
   data(){
     return {
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResult: false,
     }
   },
   methods: {
     start () {
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true;
+      this.showResult = false;
+    },
+    endGame (reactTime) {
+      this.score = reactTime;
+      this.isPlaying = false;
+      this.showResult = true;
     }
   }
 }
--- src/components/Block.vue
+++ src/components/Block.vue
@@ -24,7 +24,7 @@ export default {
         },
         stopTimer () {
             clearInterval(this.timer);
-            console.log(this.reactTime);
+            this.$emit('end', this.reactTime);
         }
     }
 
