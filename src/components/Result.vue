6bb1c7c9531a7d624fc41b70e2642ed911903b6c
mouhsineelachbi <mouhsineelachbi@gmail.com>
2020-12-13 22:24:00 +0100

    add rank and styling button

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,17 +1,35 @@
 <template>
     <div>
         <p> Reaction Time : {{ score }} ms</p>
+        <p class="rank">{{ rank }}</p>
     </div>
   
 </template>
 
 <script>
 export default {
-    props: ['score']
-
+    props: ['score'],
+    data () {
+        return {
+            rank: null,
+        }
+    },
+    mounted () {
+        if(this.score < 250){
+            this.rank = 'Ninja fingers'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = 'snail pace...'
+        }
+    }
 }
 </script>
 
 <style>
-
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
 </style>
