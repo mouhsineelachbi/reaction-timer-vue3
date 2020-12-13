d54c1733f23fb0d887d4181255c2942c1c3a9f35
mouhsineelachbi <mouhsineelachbi@gmail.com>
2020-12-13 20:28:34 +0100

    bind the delay to Block component and add disability to the button

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <h1>Mythose Reaction Timer</h1>  
-  <button @click="start">Play</button>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
 // Imports
+import Block from './components/Block.vue'
 
 
 export default {
   name: 'App',
-  components: {},
+  components: { Block },
   data(){
     return {
       isPlaying: false,
